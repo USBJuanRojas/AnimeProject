6139c006e788863a2b7b797330829ce2d2198f94
USBJuanRojas <jjrojasn@academia.usbbog.edu.co>
2025-05-10 11:24:01 -0500

    Adaptado Data y Schema para datos UUID()

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS anime_entity (
-        id CHAR(36) PRIMARY KEY,
+        id CHAR(36) NOT NULL DEFAULT (UUID()) PRIMARY KEY,
         anime_name VARCHAR(255) NOT NULL,
         anime_year VARCHAR(4) NOT NULL,
         anime_caps INTEGER NOT NULL
