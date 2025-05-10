a47c21644afea5f977d61671ae812a64d2480eca
USBJuanRojas <jjrojasn@academia.usbbog.edu.co>
2025-05-10 15:40:01 -0500

    Ya no se que pasa

--- src/main/java/com/app/animesinventory/AnimesinventoryApplication.java
+++ src/main/java/com/app/animesinventory/AnimesinventoryApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AnimesinventoryApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Backend AnimeInventory arrancando...");
 		SpringApplication.run(AnimesinventoryApplication.class, args);
+		System.out.println("Backend AnimeInventory listo en http://localhost:8080/api/v1/animes");
 	}
 
 }
